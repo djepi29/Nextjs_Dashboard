ee1e401f5670a88a10a388365923e57703c2d2cb
djepi29 <j3anpi3rr3g3org3s@email.com>
2024-05-16 09:10:23 -0400

    step 5

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const page = () => {
+  return (
+    <div>this is the invices page</div>
+  )
+}
+
+export default page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div>
+      <p>Dashboard Page</p>
+    </div>
+  )
+}
+
+export default page
